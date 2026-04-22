1023f3a63432f2758fe5167c9f1e936c7aa96bf1
Saplyn Miao <saplynx@outlook.com>
2026-04-22 18:00:30 +1000

    refactor: add tracing to qqbot-callback

--- bobot/compute/src/routes/qqbot_callback.rs
+++ bobot/compute/src/routes/qqbot_callback.rs
@@ -1,5 +1,6 @@
 use axum::{Json, extract::State, response::IntoResponse};
 use pengu::bot::callback_payload::{CallbackData, CallbackPayload, validation::ValidationResponse};
+use tracing::{Level, debug, span};
 
 use crate::primary::state::AppState;
 
@@ -7,7 +8,12 @@ pub async fn handler(
     State(state): State<AppState>,
     Json(payload): Json<CallbackPayload>,
 ) -> axum::response::Response {
+    let span = span!(Level::DEBUG, "qqbot-callback");
+
+    span.in_scope(|| debug!(message = "Handling callback", operation = ?payload.op_code));
+
     if !payload.valid() {
+        span.in_scope(|| debug!(message = "Reject because payload is invalid"));
         return http::StatusCode::BAD_REQUEST.into_response();
     }
 
@@ -18,6 +24,8 @@ pub async fn handler(
             let signature = state
                 .qqbot
                 .compute_signature(&date.bytes_iter().collect::<Vec<_>>());
+            span.in_scope(|| debug!(message = "Computed signature", signature));
+
             Json(ValidationResponse {
                 plain_token: date.plain_token,
                 signature,
